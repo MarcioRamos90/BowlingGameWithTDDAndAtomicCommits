30865cdf5a3ef8637862600e52e287d6cf13cab4
Marcio Ramos <marcio.ramos@tangotech.com.br>
2020-10-17 09:53:53 -0300

    test: gutter game

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,3 +1,11 @@
-describe('Bouling game', () => {
-    
+import { Game } from './game'
+
+describe("Bouling game", () => {
+  it("should score 0 for gutter game", () => {
+    const g: Game = new Game()
+    for (let i = 0; i < 20; i++) {
+        g.roll(0);
+    }
+    expect(g.score()).toBe(0)
+  });
 });
