20a55d115a1231f02bdfe941ddea8113a6eaa2b6
Marcio Ramos <marcio.ramos@tangotech.com.br>
2020-10-17 10:52:26 -0300

    refactor: improve game design

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -9,13 +9,20 @@ beforeEach(() => {
 describe("Bouling game", () => {
   it("should score 0 for gutter game", () => {
     rollMany(20, 0);
-    expect(g.score).toBe(0);
+    expect(g.score()).toBe(0);
   });
 
   it("should score 20 for all ones game", () => {
     rollMany(20, 1);
-    expect(g.score).toBe(20);
+    expect(g.score()).toBe(20);
   });
+
+//   it('should score 16 with a spare followed by a 3 ball', () => {
+//       g.roll(5)
+//       g.roll(5) // spare
+//       g.roll(3)
+//       expect(g.score).toBe(16)
+//   });
 
   function rollMany(n: number, pins: number): void {
     for (let i = 0; i < n; i++) {
