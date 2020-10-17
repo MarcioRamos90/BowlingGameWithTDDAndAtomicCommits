1b28c23ff0302bd3756aee2ebe6389fa9462ec64
Marcio Ramos <marcio.ramos@tangotech.com.br>
2020-10-17 11:40:02 -0300

    test: strike score

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -23,9 +23,17 @@ describe("Bouling game", () => {
     expect(g.score()).toBe(16);
   });
 
+  it("should score 24 for a strike followed by 3 and 4 balls", () => {
+    g.roll(10); // strike
+    g.roll(3);
+    g.roll(4);
+    rollMany(16, 0);
+    expect(g.score()).toBe(24);
+  });
+
   function rollSpare() {
     g.roll(5);
-    g.roll(5); // spare
+    g.roll(5);
   }
 
   function rollMany(n: number, pins: number): void {
