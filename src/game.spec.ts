8dae2fa3a201a8e5e8cac3c48d933159c3ce5843
Marcio Ramos <marcio.ramos@tangotech.com.br>
2020-10-17 11:31:35 -0300

    refactor: extract method to roll a spare

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -17,12 +17,16 @@ describe("Bouling game", () => {
     expect(g.score()).toBe(20);
   });
 
-  it('should score 16 with a spare followed by a 3 ball', () => {
-      g.roll(5)
-      g.roll(5) // spare
-      g.roll(3)
-      expect(g.score()).toBe(16)
+  it("should score 16 with a spare followed by a 3 ball", () => {
+    rollSpare();
+    g.roll(3);
+    expect(g.score()).toBe(16);
   });
+
+  function rollSpare() {
+    g.roll(5);
+    g.roll(5); // spare
+  }
 
   function rollMany(n: number, pins: number): void {
     for (let i = 0; i < n; i++) {
