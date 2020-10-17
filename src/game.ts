bbf4f514046055c01b0227f0018200b9bea3826d
Marcio Ramos <marcio.ramos@tangotech.com.br>
2020-10-17 11:46:31 -0300

    refactor: improve game design with socre helper methods

--- src/game.ts
+++ src/game.ts
@@ -8,17 +8,29 @@ export class Game {
     for (let frame = 0; frame < 10; frame++) {
       if (this.rolls[frameIndex] === 10) {
         // strike
-        score += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+        score += 10 + this.strikeBonus(frameIndex);
         frameIndex++;
       } else if (this.isSpare(frameIndex)) {
-        score += 10 + this.rolls[frameIndex + 2]; // first play of next frame
+        score += 10 + this.spareBonus(frameIndex);
         frameIndex += 2;
       } else {
-        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+        score += this.sumOfBallsInFrame(frameIndex)
         frameIndex += 2;
       }
     }
     return score;
+  }
+
+  private sumOfBallsInFrame(frameIndex: number): number {
+      return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+  }
+
+  private strikeBonus(frameIndex: number): number {
+    return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+  }
+
+  private spareBonus(frameIndex: number): number {
+    return this.rolls[frameIndex + 2];
   }
 
   private isSpare(frameIndex: number): boolean {
